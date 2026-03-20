ac1a534405554c26d6b1f7e7de9268f3ed25f424
Satyam Raghuvanshi <satyamraghuvanshi220ct@gmail.com>
2026-03-20 17:38:55 +0530

    feat: Add basic Java program demonstrating array declaration, input, and output.

---
+++ Arrays/ArraysCC.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+
+public class ArraysCC {
+    public static void main(String[] args) {
+         int marks[] = new int[100];
+
+         Scanner sc = new Scanner(System.in);
+          //int phy;
+          //phy = sc.nextInt();
+
+         marks[0] = sc.nextInt();
+         marks[1] = sc.nextInt();
+         marks[2] = sc.nextInt();
+
+         System.out.println("phy:"+ marks[0]);
+         System.out.println("chem:"+ marks[1]);
+         System.out.println("math:"+ marks[2]);
+         
+    }
+}
