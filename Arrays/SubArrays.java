4679d9bb130b9cfe52706c970aab55efaa393ed7
Satyam Raghuvanshi <satyamraghuvanshi220ct@gmail.com>
2026-05-24 01:25:47 +0530

    feat: Implement binary search, reverse array, print pairs, and print subarrays in Java

---
+++ Arrays/SubArrays.java
@@ -0,0 +1,24 @@
+package Arrays;
+//Print SubArrays
+
+public class SubArrays {
+    public static void printSubArrays(int numbers[]){
+        for(int i=0; i<numbers.length; i++){
+            int start = i;
+            for(int j=i+1; j<numbers.length; j++){
+                int end = j;
+                for(int k=start; k<=end; k++){ //print
+                    System.out.print(numbers[k]+" ");//subarray
+                }
+                System.out.println();
+            }
+            System.out.println();
+            }
+
+            System.out.println("Total SubArrays: "+(numbers.length*(numbers.length+1)/2));
+        }
+        public static void main(String args[]){
+            int numbers[]={2,4,6,8,10};
+            printSubArrays(numbers);
+        }
+    }
