caaed22d00cbe60b02b2ee2153a3524d20c51079
raghuvanshi-sec <satyamraghuvanshi220ct@gmail.com>
2026-07-31 16:48:15 +0530

    feat: add implementation examples for inheritance and hybrid inheritance concepts

---
+++ OOPs/HybridInheritance.java
@@ -0,0 +1,100 @@
+public class HybridInheritance {
+
+    public static void main(String args[]) {
+        Tuna tuna = new Tuna();
+        tuna.eat();
+        tuna.swim();
+        tuna.breathe();
+
+        Shark shark = new Shark();
+        shark.eat();
+        shark.swim();
+        shark.breathe();
+
+        Peacock peacock = new Peacock();
+        peacock.eat();
+        peacock.fly();
+        peacock.dance();
+
+        Dog dog = new Dog();
+        dog.eat();
+        dog.walk();
+        dog.bark();
+
+        Monkey monkey = new Monkey();
+        monkey.eat();
+        monkey.walk();
+        monkey.climb();
+
+        Human human = new Human();
+        human.eat();
+        human.walk();
+        human.breed();
+    }
+}
+
+class Animal {
+    String color;
+
+    void eat() {
+        System.out.println("eats");
+    }
+}
+
+class Fish extends Animal {
+    int fins;
+
+    void swim() {
+        System.out.println("swims in water");
+    };
+}
+
+class Tuna extends Fish {
+    void breathe() {
+        System.out.println("breathes with gills");
+    }
+}
+
+class Shark extends Fish {
+    void breathe() {
+        System.out.println("breathes with nostrils");
+    }
+}
+
+class Birds extends Animal {
+    void fly() {
+        System.out.println("fly");
+    }
+}
+
+class Peacock extends Birds {
+    void dance() {
+        System.out.println("dances");
+    }
+}
+
+class Mammals extends Animal {
+    int legs;
+
+    void walk() {
+        System.out.println("walks");
+    }
+}
+
+class Dog extends Mammals {
+    void bark() {
+        System.out.println("barks");
+    }
+}
+
+class Monkey extends Mammals {
+    void climb() {
+        System.out.println("climbs");
+    }
+}
+
+class Human extends Mammals {
+    void breed() {
+        System.out.println("breeds");
+    }
+}
--- OOPs/Inheritance.java
+++ OOPs/Inheritance.java
@@ -1,42 +1,70 @@
 public class Inheritance {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Mammals dog = new Mammals();
-        dog.legs = 4;
+        dog.walk();
         dog.eat();
         dog.breathe();
-        System.out.println(dog.legs);
 
+        Fish fish = new Fish();
+        fish.swim();
+        fish.eat();
+        fish.breathe();
+
+        Bird bird = new Bird();
+        bird.fly();
+        bird.eat();
+        bird.breathe();
     }
 }
 
-
-//Base Class
-class Animal{
+// Base Class
+class Animal {
     String color;
 
-    void eat(){
+    void eat() {
         System.out.println("eats");
     }
 
-    void breathe(){
+    void breathe() {
         System.out.println("breathes");
     }
 }
 
-//Derived Class
+class Fish extends Animal {
+    int fins;
+
+    void swim() {
+        System.out.println("swims in water");
+    }
+}
+
+class Mammals extends Animal {
+    void walk() {
+        System.out.println("walks");
+    }
+}
+
+class Bird extends Animal {
+    void fly() {
+        System.out.println("flies");
+    }
+}
+
+// Derived Class
 // class Fish extends Animal{
-//     int fins;
+// int fins;
 
-
-//     void swim(){
-//         System.out.println("swims in water");
-//     }
+// void swim(){
+// System.out.println("swims in water");
+// }
 // }
 
-class Mammals extends Animal{
-    int legs;
-}
+// class Mammals extends Animal{
+// void walk(){
+// System.out.println("walks");
+// }
+// }
 
-class Dog extends Mammals{
-    String breed;
-}
+// class Dog extends Mammals{
+// String breed;
+// }
