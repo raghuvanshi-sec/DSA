f6de1b854cdda2f505c270e734830ee7582a908f
Satyam Raghuvanshi <satyamraghuvanshi220ct@gmail.com>
2026-08-24 12:41:02 +0530

    Time: 1 ms (81.47%), Space: 44.5 MB (27.03%) - LeetHub

---
+++ 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.java
@@ -0,0 +1,52 @@
+class Solution {
+
+    public List<List<Integer>> queensAttacktheKing(
+            int[][] queens, int[] king) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        boolean[][] board = new boolean[8][8];
+
+        // Store queen positions
+        for (int[] queen : queens) {
+            board[queen[0]][queen[1]] = true;
+        }
+
+        // 8 directions
+        int[][] directions = {
+            {-1, -1},  // upper-left
+            {-1, 0},   // up
+            {-1, 1},   // upper-right
+            {0, -1},   // left
+            {0, 1},    // right
+            {1, -1},   // lower-left
+            {1, 0},    // down
+            {1, 1}     // lower-right
+        };
+
+        // Check all 8 directions
+        for (int[] dir : directions) {
+
+            int row = king[0] + dir[0];
+            int col = king[1] + dir[1];
+
+            while (row >= 0 && row < 8 &&
+                   col >= 0 && col < 8) {
+
+                // First queen found
+                if (board[row][col]) {
+
+                    ans.add(Arrays.asList(row, col));
+
+                    // Stop this direction
+                    break;
+                }
+
+                row += dir[0];
+                col += dir[1];
+            }
+        }
+
+        return ans;
+    }
+}
